4ba32af59bdeb8198bc53897082b2578683e529a
Lance Barney <lbarney@littleappletech.com>
2016-07-12 17:07:34 -0600

    got data to the db

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,5 +1,13 @@
 angular.module('chatApp').controller('mainCtrl', function($scope, mainServ){
+var newChat = {};
 
 
+$scope.postChat = function(){
+	newChat.body = $scope.newChat
+	mainServ.postChat(newChat)
+	.then(function(res){
+		$scope.chat = res.data
+	})
+}
 
 });
--- public/mainServ.js
+++ public/mainServ.js
@@ -1,3 +1,14 @@
 angular.module('chatApp').service('mainServ', function($http){
 
+	this.postChat= function(data){
+		return $http({
+			method: 'POST',
+			url: '/chat',
+			data: data
+		}).then(function(res){
+			console.log(res.data);
+			return res.data;
+		})
+	};
+
 });

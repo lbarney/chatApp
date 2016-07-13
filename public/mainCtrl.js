6f3b788bc07570ad5894062dfe2524861bddc9ed
Lance Barney <lbarney@littleappletech.com>
2016-07-13 07:51:24 -0600

    update

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -6,14 +6,15 @@ $scope.postChat = function(){
 	newChat.body = $scope.newChat
 	mainServ.postChat(newChat)
 	.then(function(res){
-		$scope.chat = res.data
+		$scope.chat = res.data;
+		$scope.getAllChats();
 	})
 },
 $scope.getAllChats = function(){
 	mainServ.getServChats()
 	.then(function(res){
 		$scope.chats = res
-		console.log(res)
+		console.log(res);
 	})
 }
 

63dc269dc7bae580a8ef2992a284678b20ff7027
Lance Barney <lbarney@littleappletech.com>
2016-07-12 20:03:59 -0600

    sending and getting data from the database

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,12 +1,19 @@
 angular.module('chatApp').controller('mainCtrl', function($scope, mainServ){
 var newChat = {};
-
+$scope.chats;
 
 $scope.postChat = function(){
 	newChat.body = $scope.newChat
 	mainServ.postChat(newChat)
 	.then(function(res){
 		$scope.chat = res.data
+	})
+},
+$scope.getAllChats = function(){
+	mainServ.getServChats()
+	.then(function(res){
+		$scope.chats = res
+		console.log(res)
 	})
 }
 
--- public/mainServ.js
+++ public/mainServ.js
@@ -1,5 +1,8 @@
 angular.module('chatApp').service('mainServ', function($http){
 
+
+
+
 	this.postChat= function(data){
 		return $http({
 			method: 'POST',
@@ -9,6 +12,15 @@ angular.module('chatApp').service('mainServ', function($http){
 			console.log(res.data);
 			return res.data;
 		})
-	};
+	},
+	this.getServChats = function(){
+		return $http({
+			method: 'GET',
+			url: '/chat'
+			}).then(function(res){
+				console.log(res.data)
+				return (res.data)
+		})
+	}
 
 });
